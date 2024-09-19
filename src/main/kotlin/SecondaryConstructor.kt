8ca10ae12eee8487cc2c7699795a354af07f0230
nudriin <mrsunnysummer@gmail.com>
2024-09-19 16:26:35 +0700

    ch_6_secondary_constructor

---
+++ src/main/kotlin/SecondaryConstructor.kt
@@ -0,0 +1,30 @@
+package com.nudriin
+
+class Animal2(name: String, weight: Double, age: Int) {
+    val name: String
+    val weight: Double
+    val age: Int
+    var isMammal: Boolean
+
+    init {
+        this.weight = if(weight < 0) 0.1 else weight
+        this.age = if(age < 0) 0  else age
+        this.name = name
+        this.isMammal = false
+    }
+
+    // Secondary constructor
+    constructor(name: String, weight: Double, age: Int, isMammal: Boolean) : this(name, weight, age) {
+        this.isMammal = isMammal
+    }
+}
+
+fun main() {
+    // akan memanggil secondary constructor
+    val dicodingCat = Animal2("Dicoding Miaw", 2.5, 2, true)
+    println("Nama: ${dicodingCat.name}, Berat: ${dicodingCat.weight}, Umur: ${dicodingCat.age}, mamalia: ${dicodingCat.isMammal}")
+
+    // akan memanggil primary constructor
+    val dicodingBird = Animal2("Dicoding tweet", 0.5, 1)
+    println("Nama: ${dicodingBird.name}, Berat: ${dicodingBird.weight}, Umur: ${dicodingBird.age}, mamalia: ${dicodingBird.isMammal}")
+}
